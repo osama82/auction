a839b40d3b1d4cfd65c458d6471a4dfdcb7b4b0f
osama alnajar <osama.alnajar82@gmail.com>
2017-09-26 14:05:39 +0200

    makebid

--- Auction.java
+++ Auction.java
@@ -69,8 +69,8 @@ public class Auction
     {
         Lot selectedLot = getLot(lotNumber);
         if(selectedLot != null) {
-            Bid bid = new Bid(bidder, value);
-            boolean successful = selectedLot.bidFor(bid);
+            //Bid bid = new Bid(bidder, value);
+            boolean successful = selectedLot.bidFor(new Bid(bidder, value));
             if(successful) {
                 System.out.println("The bid for lot number " +
                                    lotNumber + " was successful.");
